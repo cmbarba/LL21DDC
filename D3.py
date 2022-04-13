3b0954acabd8d965dce253ceb4be9e94e5e4ea10
Cindi Michael Barba <cmbarba@protonmail.com>
2022-04-13 01:06:29 -0400

    Removed import; switched to for loop, break after finding earliest match

--- D3.py
+++ D3.py
@@ -2,9 +2,6 @@
 
 # Provided
 
-from turtle import clear
-
-
 layout = [
     ["O","e","e",None, "e","o","e",None, "o","o","E"], # row 1
     ["O","o","e",None, "e","o","e",None, "e","o","E"], # row 2
@@ -18,21 +15,15 @@ layout = [
 
 #Conditions are consecutive E,e or e,E
 
-l1 = len(layout)
-#print(l1)
-l2 = len(layout[0])
-#print(l2)
+i = 1
 
-i = 0
-while i < l1:
-    j = 0
-    while j < (l2-1):
-        #print(f"[i,j] = [{i},{j}]")
-        if layout[i][j] == "E":
-            if layout[i][(j+1)] =="e":
-                print(f"Seat at [{i},{j}]")
-        elif layout[i][j] =="e":
-            if layout[i][(j+1)] =="E":
-                print(f"Seat at [{i},{j}]")            
-        j+=1
-    i+=1
+for x in layout:
+    if x[0]=="E":
+        if x[1]=="e":
+            print(f"Seat at Row {i} Seat 1 (Index {(i-1)},0")
+            break
+    if x[-1]=="E":
+        if x[-2]=="e":
+            print(f"Seat at Row {i} Seat 11 (Index {(i-1)},-1)")
+            break
+    i +=1
