531735ad3d7403c8dffd8783e8149f6f8c1fa59d
Cindi Michael Barba <cmbarba@protonmail.com>
2022-04-15 00:31:01 -0400

    Added

---
+++ D5.py
@@ -0,0 +1,29 @@
+## Day 4 Challenge ##
+
+# Provided
+
+# dictionary where 
+landmarks = {
+    "Big Ben": 12,
+    "Tower Bridge": 25,
+    "Buckingham Palace": 15,
+    "Madame Tussauds": 25,
+    "London Eye": 40,
+    "Tower of London": 25,
+    "Emirates Air Line cable car": 16,
+    "London Transport Museum": 7,
+    "Wembley Stadium": 8,
+    "Hyde Park": 0,
+    "The View from The Shard": 14
+}
+
+# Work
+
+places = []
+
+for x,y in landmarks.items(): #items() gives access to both keys and values, otherwise just keys
+    print(x,y)
+    if y < 15:
+        places.append([x,y])
+
+print (places)
