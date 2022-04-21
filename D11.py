5e40fea7dfc64b27a5220a5728477d1ce0af08b6
Cindi Michael Barba <cmbarba@protonmail.com>
2022-04-21 02:32:25 -0400

    Added challenge question as comment

--- D11.py
+++ D11.py
@@ -8,5 +8,6 @@ df.head()
 
 # Work
 
+#Which neighborhood has the biggest difference between maximum and minimum property price?
 df1 = df.groupby(['neighborhood']).agg({'price' : ['max','min']})
 (df1['price']['max'] - df1['price']['min']).sort_values(ascending=False).head(1)
