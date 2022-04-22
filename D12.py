2628f6e9c12eb3746633ed31765c524a2ec69927
Cindi Michael Barba <cmbarba@protonmail.com>
2022-04-22 01:15:07 -0400

    Added alternate method for season filter

--- D12.py
+++ D12.py
@@ -8,5 +8,6 @@ df = pd.read_csv('aus_weather.csv')
 # Work
 
 d1 = df[df['season'].isin('summer','winter')] #originally used .str.contains('winter|summer') instead of .isin()
+d1 = df[(df['season'].isin('summer')) | df['season'].isin('winter')] #alternate method with | as explicit or
 d2 = d1.groupby(['Location','season']).agg({'Temp9am' : 'mean'}).unstack(level=1)
 (d2['Temp9am']['summer'] - d2['Temp9am']['winter'])
