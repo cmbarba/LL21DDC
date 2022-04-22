b16284984d7cfe43d974fd10decf2e02de48d3ea
Cindi Michael Barba <cmbarba@protonmail.com>
2022-04-22 01:04:33 -0400

    Switched to .isin() filter from .str.contains()

--- D12.py
+++ D12.py
@@ -7,6 +7,6 @@ df = pd.read_csv('aus_weather.csv')
 
 # Work
 
-d1 = df[df['season'].str.contains('winter|summer')]
+d1 = df[df['season'].isin('summer','winter')] #originally used .str.contains('winter|summer') instead of .isin()
 d2 = d1.groupby(['Location','season']).agg({'Temp9am' : 'mean'}).unstack(level=1)
 (d2['Temp9am']['summer'] - d2['Temp9am']['winter'])
