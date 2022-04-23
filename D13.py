dbd9d29e6f4703aa9dd6de63449c4c5c4b14cf34
Jon-Ledo <70028256+Jon-Ledo@users.noreply.github.com>
2022-04-23 07:03:29 -0400

    Update D13.py

--- D13.py
+++ D13.py
@@ -17,3 +17,16 @@ df['Temp9am'].mode()
 
 #Using a boxplot, does it ever rain (using variable Rainfall) in Melbourne? If yes, what was the highest daily amount recorded?
 df['Rainfall'].max() #essentially gives answer since only one option has max rainfall over 80mm
+
+# #################
+# Solution 2
+import pandas as pd
+df = pd.read_csv('aus_weather.csv')
+
+# the most likely temperature at 9am (Temp9am) in Melbourne
+df = df[df["Location"] == "Melbourne"].agg('Temp9am')
+df.plot(kind='hist') # Highest frequency shown between 12.5 and 15 degrees
+
+# does it ever rain (using variable Rainfall) in Melbourne?
+df = df[df["Location"] == "Melbourne"].agg('Rainfall')
+df.plot(kind='box') # obviously rains, highest above 80mm
